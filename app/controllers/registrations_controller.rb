f8c04b71e40ec2802546ed3134c0ca9aa4fa79c7
kunmi02 <mrkhunmie@gmail.com>
2021-01-22 17:24:36 +0100

    fix linter errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
+  private
 
-    private
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirm)
+  end
 
-    def sign_up_params
-        params.require(:user).permit(:name, :email, :password, :password_confirm)
-    end
-
-    def sign_up_params
-        params.require(:user).permit(:name, :email, :password, :password_confirm, :current_password)
-    end
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirm, :current_password)
+  end
 
 end
