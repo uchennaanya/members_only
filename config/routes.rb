ed4b4fce28ad8c2c9f311a4070a63bfc17cb8e50
nna nna 19 <uchennaongit@gmail.com>
2021-01-22 15:01:05 +0100

    Modify application_record.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   
-  devise_for :posts, only: [:new, :create, :destroy, :edit]
+  devise_for :posts, only: [:new, :create, :destroy, :edit, :new, :index]
 
-  resources :posts, only: [:show, :index]
+  resources :posts, only: [:show, :index, :edit, :new]
   root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
