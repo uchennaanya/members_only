5c88c85a9de419e7d7b301da3891109b849301bf
kunmi02 <mrkhunmie@gmail.com>
2021-01-22 13:23:33 +0100

    Routes path resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index, :edit, :destroy]
   root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
