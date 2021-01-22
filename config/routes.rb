883498af487366751d1d33fcb82dea27ead38619
kunmi02 <mrkhunmie@gmail.com>
2021-01-22 13:41:03 +0100

    restrict routes to logged in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :posts, only: [:new, :create, :index, :edit, :destroy]
+  
+  devise_for :posts, only: [:new, :create, :destroy, :edit]
+
+  resources :posts, only: [:show, :index]
   root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
