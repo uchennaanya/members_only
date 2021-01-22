9225e94c972aa59fd7af9692e06519c9961e5443
kunmi02 <mrkhunmie@gmail.com>
2021-01-22 16:13:20 +0100

    Routes protection

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  
-  devise_for :posts, only: [:new, :create, :destroy, :edit, :new, :index]
 
-  resources :posts, only: [:show, :index, :edit, :new]
+  # devise_for :posts, only: [:new, :create, :destroy, :edit, :new, :index, :show]
+
+  resources :posts, only: [:new, :create, :destroy, :edit, :index, :show, :update]
   root to: 'posts#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
